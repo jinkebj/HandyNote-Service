42c32099ab538c46b5d4a415d1436e05e259bbd9
Jin Ke <jinkebj@126.com>
2017-08-04 11:19:08 +0800

    support set backend mongodb url through env HANDYNOTE_MONGO_URL

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   SERVER_PORT: 3000,
-  MONGO_URL: 'mongodb://handy:handypassw0rd@localhost/HandyNote'
+  MONGO_URL: process.env.HANDYNOTE_MONGO_URL || 'mongodb://localhost/HandyNote'
 }
